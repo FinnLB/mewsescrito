785d8b883c852f57a5062cc9e038c355a5203afb
Finn Brodmann <56689087+FinnLB@users.noreply.github.com>
2022-06-14 20:42:49 +0200

    Add files via upload

---
+++ src/main/java/de/unidue/ltl/edu_collaborations/mews/WekaClassificationExperiment.java
@@ -0,0 +1,11 @@
+package de.unidue.ltl.edu_collaborations.mews;
+
+
+public class WekaClassificationExperiment {
+
+	public static void main(String[] args) throws Exception {
+		de.unidue.ltl.escrito.examples.basicexamples.shortanswer.WekaClassificationExample.main(args);
+		
+	}
+
+}
